04726e303cbcc6d8b2150b6f627c73901218cc6b
Namitha Reddy <89120803+GNamithaReddy@users.noreply.github.com>
2022-03-02 17:48:32 +0530

    updated

--- simulator.py
+++ simulator.py
@@ -12,6 +12,9 @@ import re
 
 base_address = 0x10000000  # 4KB
 PC = 0
+bne_stat=''
+beq_stat=''
+j_stat=''
 
 Reg = {"zero": 0, "ra": 0, "sp": 0, "gp": 0, "tp": 0, "t0": 0, "t1": 0, "t2": 0, "s0": 0, "s1": 0,
        "a0": 0, "a1": 0, "a2": 0, "a3": 0, "a4": 0, "a5": 0, "a6": 0, "a7": 0, "s2": 0, "s3": 0, "s4": 0,
@@ -84,10 +87,10 @@ def perform_instructions(instruction, PC):
 
         if Reg[reg1] != Reg[reg2]:             #???
             bne_flag = instruction[3]
-            PC = main[bne_flag]    #main
+            PC = main[bne_stat]    #main
 
         else:
-            bne_flag = ''
+            bne_stat = ''
             PC += 1
 
     elif instruction[0] == 'lui':  # [ 'lui','a0','0x10101'] upper 20bits
@@ -122,13 +125,13 @@ def perform_instructions(instruction, PC):
         else:
             reg1 = instruction[1]
             reg2 = instruction[2]
-            if Reg[reg1] == Reg[reg2]:
-                beq_flag = instruction[3]
-            PC = main[beq_flag]     #??
+            if (Reg[reg1] == Reg[reg2]):
+                beq_stat = instruction[3]
+                PC = main[beq_stat]     #??
 
-         '''   else:
-            beq_flag = ''
-            PC += 1  '''
+            else:
+                beq_stat = ''
+                PC += 1 
 
     elif instruction[0] == 'j':
 
@@ -139,7 +142,7 @@ def perform_instructions(instruction, PC):
             j_flag = instruction[1]
             PC = main[j_flag]
 
-        elif (instruction[0] == 'addi'):
+    elif (instruction[0] == 'addi'):
 
         if len(instruction) != 4:
             print("syntax error at line number %d", PC)
@@ -153,7 +156,7 @@ def perform_instructions(instruction, PC):
             else:
                 Reg[reg1] = Reg[reg2] + addend
                 PC += 1
-        elif (instruction[0] == 'andi'):
+    elif (instruction[0] == 'andi'):
 
             if (len(instruction) != 4):
                 print("syntax error at line number %d", PC)
@@ -165,7 +168,7 @@ def perform_instructions(instruction, PC):
                 Reg[reg1] = hex(int(Reg[reg2], 16) & int(anded, 16))
                 PC +=1
 
-        elif (instruction[0] == 'and'):
+    elif (instruction[0] == 'and'):
 
             if (len(instruction) != 4):
                 print("syntax error at line number %d", PC)
@@ -177,61 +180,123 @@ def perform_instructions(instruction, PC):
                 Reg[reg1] = hex(int(Reg[reg2], 16) & int(Reg[reg3], 16))
                 PC = PC + 1
 
-        return PC
+    return PC
 
-
-def data_part(file):
-    data_set = []
-    pattern = re.compile('\s*[,:|\s+]\s*')
+def all_instructions(file):
+    all_instructions=[]
+    pattern = re.compile('\s*[,|\s+]\s*')
     for line in file:
         data = remove_comments(line).strip()
-        if data == ".text":
-            break
-        elif len(data) != 0:
-            if data != "start:" and data != ".data":
-                data_set.append([x for x in pattern.split(data) if x])
-    return data_set
+        if len(data) != 0:
+            instruction = [x for x in pattern.split(data) if x]
+            all_instructions.append(instruction)
+    return all_instructions
+
 
 def remove_comments(file):
     return re.sub('#.*', '', file)
 
 
-def instructions(file):
-    instructions_set = []
-    pattern = re.compile('\s*[,:|\s+]\s*')
-    flag = False
-    for line in file:
-        data = remove_comments(line).strip()
-        if data == ".text":
-            flag = True
-        elif len(data) != 0 and flag:
-            instruction = [x for x in pattern.split(data) if x]
-            if len(instruction) > 1:
-                instructions_set.append(instruction)
-    return instructions_set
 
 
-def all_instructions(file):
-    instructions_set = []
-    pattern = re.compile('\s*[,:|\s+]\s*')
-    flag = False
-    for line in file:
-        data = remove_comments(line).strip()
-        if data == ".text":
-            flag = True
-        elif len(data) != 0 and flag:
-            instruction = [x for x in pattern.split(data) if x]
-            if len(instruction) == 1:
-                instructions_set.append(instruction)
-    return instructions_set
+fileopen = open("riscv3.asm", "r+")
+instructionslist=all_instructions(fileopen)
+
+data_and_text={'data':[],'text':[]}       # dictionary containing two lists data list and text list which contain respective instructions
+data_index=0
+text_index=0
+
+for i in range(len(instructionslist)):
+    if(instructionslist[i][0]=='.data'):
+        data_index=i
+    elif(instructionslist[i][0]=='.text'):
+        text_index=i
+
+for i in range(data_index+1,text_index):
+    data_and_text['data'].append(instructionslist[i])
+
+for i in range(text_index+1,len(instructionslist)):
+    if(instructionslist[i][0]!='main:'):
+        data_and_text['text'].append(instructionslist[i])
+
+data={'.word':[]}
+count=0
+for ins in data_and_text['data']:
+    if(ins[1]=='.word'):
+        for i in range(2,len(ins)):
+           data['.word'].append(int(ins[i]))
+           count+=1 
 
 
-# def operations(instructions):
-# print(len(Registers))
-# testing
-fileopen = open("risc.asm", "r+")
-print(instructions(fileopen))
-fileopen.seek(0)
-print(data_part(fileopen))
-fileopen.seek(0)
-print(all_instructions(fileopen))
+main={}                 #dictionary which contains labels as keys and corresponding numbers as values
+count=0
+
+for ins in data_and_text['text']:
+    if(len(ins)==1):                       # label:
+        ins[0]=ins[0][:-1]                 #label
+        main[ins[0]]=count                 #main[label]=0  main={'label':0}
+    else:
+        count+=1
+
+for ins in data_and_text['text']:
+    if(ins[0] in main.keys()):
+        data_and_text['text'].remove(ins)
+
+for ins in data_and_text['text']:
+    print(ins)                                             #prints list of all list instructions without labels
+
+print(Reg)                                                 #prints Reg list
+print(data['.word'])                                                # prints elements of array                                 
+print(data_and_text['data'])
+print(main)                                                #prints labels inside text part after main 
+print("Press 1 to RUN file")
+print("Press 2 to RUN step by step")
+
+choice=int(input())
+count=0
+
+if(choice==1):
+    while(PC!=len(data_and_text['text'])-1):
+        print(PC)
+        count+=1
+        PC= perform_instructions(data_and_text['text'][PC],PC)
+        
+        if(PC>len(data_and_text['text'])):
+            print("Sorry terminating the program due to unexpected error :(")
+        
+    for Registers in Reg.keys():
+        print(Registers+": "+str(Reg[Registers]))
+    
+    for i in range(len(data['.word'])):
+        print(hex((base_address+4*i))+": "+str(data['.word'][i]))
+    print(count)
+
+elif(choice==2):
+    
+    print('1.Run command || 2.Show registers || 3.Show Memory || 4.exit')
+
+    option = int(input())
+
+    while(True):                                  #keeps on running until we stop giving some number
+
+        if(option==1):
+            PC = perform_instructions(data_and_text['text'][PC],PC)
+
+        elif(option==2):
+            for register in Reg.keys():
+                    print(register+": "+str(Reg[register]))
+
+        elif(option==3):
+            for i in range(len(data['.word'])):
+                print(hex((base_address+4*i))+": "+str(data['.word'][i]))
+        
+        elif(option==4):
+            break
+
+        if(PC>len(data_and_text['text'])):
+            print("Sorry terminating the program due to unexpected error :(")
+            break   
+        
+        print('1.Run command || 2.Show registers || 3.Show Memory || 4.exit')
+
+        int_option = int(input())    
