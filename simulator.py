190c87d8af92cde9c4aee3faf9877d24bdd62b37
Pooja Sree <84029615+poojasree06@users.noreply.github.com>
2022-03-03 23:57:10 +0530

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,14 +1,14 @@
 import re
 import sys
 
-# instructions=['add','sub,'lw','sw','lui','and','addi','beq','bne','bge','ble','li']
+# instructions=['add','sub,,'addi','lw','sw','lui','slt','and','andi','beq','bne','bge','ble','li','slli','sll','j']
 
-# add,sub,mul?,div?             arithmetic                4
-# and,or                        boolean                   2  -->done
+# add,sub,addi                  arithmetic                3  -->done
+# and,slli,andi,sll             boolean                   4  -->done
 # lw,sw,lui                     memory                    3  -->done
-# addi,li                       immediate                 2  -->done
-# bne,beq,bge,ble,bgt           conditional jumps         2  -->done
+# bne,beq,bge,ble,bgt           conditional jumps         5  -->done
 # j                             unconditional jumps       1  -->done
+
 
 base_address = 0x10010000  # 4KB
 PC = 0
@@ -54,7 +54,7 @@ def perform_instructions(instruction, PC):
             elif type(Reg[reg1]) == str and type(Reg[reg2]) == int:
                 Reg[reg0] = hex(int(Reg[reg1], 16) + Reg[reg2])
             elif type(Reg[reg2]) == str and type(Reg[reg1]) == int:
-                Reg[reg0] = hex(int(Reg[reg2], 16) + Reg[reg2])
+                Reg[reg0] = hex(int(Reg[reg2], 16) + Reg[reg1])
             else:
                 Reg[reg0] = Reg[reg1] - Reg[reg2]
             PC += 1
@@ -96,26 +96,9 @@ def perform_instructions(instruction, PC):
                     data['.word'][index] = Reg[reg1]
             PC += 1
 
-    elif instruction[0] == 'bne':
-        if len(instruction) != 4:
-            print("syntax error at line number %d", PC)
-
-        else:
-            reg1 = instruction[1]
-            reg2 = instruction[2]
-        if Reg[reg1] != Reg[reg2]:
-            bne_stat = instruction[3]
-            PC = main[bne_stat]  # main
-
-        else:
-            bne_stat = ''
-            PC += 1
-
     elif instruction[0] == 'lui':  # [ 'lui','a0','0x10101'] upper 20bits
-
         if len(instruction) != 3:
             print("syntax error at line number %d", PC)
-
         else:
             reg1 = instruction[1]
             Reg[reg1] = hex(int(instruction[2] + '000', 16))
@@ -133,6 +116,19 @@ def perform_instructions(instruction, PC):
                 Reg[reg1] = 1
             PC += 1
 
+    elif instruction[0] == 'bne':
+        if len(instruction) != 4:
+            print("syntax error at line number %d", PC)
+        else:
+            reg1 = instruction[1]
+            reg2 = instruction[2]
+        if Reg[reg1] != Reg[reg2]:
+            bne_stat = instruction[3]
+            PC = main[bne_stat]  # main
+        else:
+            bne_stat = ''
+            PC += 1
+            
     elif instruction[0] == 'bge':
         if len(instruction) != 4:
             print("syntax error at line number %d", PC)
@@ -194,11 +190,28 @@ def perform_instructions(instruction, PC):
             reg2 = instruction[2]
             reg3 = int(instruction[3])
             if type(Reg[reg2]) == str and Reg[reg2][0:2] == '0x':
-                print(hex(int(Reg[reg2], 16) << reg3))
+                # print(hex(int(Reg[reg2], 16) << reg3))
                 Reg[reg1] = hex(int(Reg[reg2], 16) << reg3)
             else:
                 Reg[reg1] = Reg[reg2] << reg3
-                print(Reg[reg1])
+            #  print(Reg[reg1])
+        PC += 1
+
+    elif instruction[0] == 'sll':
+        if len(instruction) != 4:
+            print("syntax error at line number %d", PC)
+        else:
+            reg1 = instruction[1]
+            reg2 = instruction[2]
+            reg3 = int(instruction[3])
+            if type(Reg[reg2]) == str and Reg[reg2][0:2] == '0x':
+                # print(hex(int(Reg[reg2], 16) << reg3))
+                Reg[reg1] = hex(int(Reg[reg2], 16) << reg3)
+            elif type(Reg[reg3]) == str and Reg[reg3][0:2] == '0x':
+                Reg[reg1] = hex(reg2 << int(Reg[reg3], 16))
+            else:
+                Reg[reg1] = Reg[reg2] << reg3
+            #  print(Reg[reg1])
         PC += 1
 
     elif instruction[0] == 'j':
@@ -218,7 +231,7 @@ def perform_instructions(instruction, PC):
                 Reg[reg1] = hex(int(Reg[reg2], 16))
             else:
                 Reg[reg1] = reg2
-      #  print(Reg)
+        #  print(Reg)
         PC += 1
 
     elif instruction[0] == 'addi':
@@ -229,11 +242,11 @@ def perform_instructions(instruction, PC):
             reg2 = instruction[2]
             addend = int(instruction[3])
             if type(Reg[reg2]) == str and Reg[reg2][0:2] == '0x':
-                print(hex(int(Reg[reg2], 16) + addend))
+                # print(hex(int(Reg[reg2], 16) + addend))
                 Reg[reg1] = hex(int(Reg[reg2], 16) + addend)
             else:
                 Reg[reg1] = Reg[reg2] + addend
-               # print(Reg[reg1])
+            # print(Reg[reg1])
         PC += 1
 
     elif instruction[0] == 'andi':
@@ -274,13 +287,32 @@ def all_instructions(file):
 def remove_comments(file):
     return re.sub('#.*', '', file)
 
+def show_registers():
+    i = 0
+    print()
+    for reg in Reg.keys():
+        print(reg + " (x" + str(i) + ") " + ": " + str(Reg[reg]))
+        i = i + 1
+    print()
+
+
+def show_memory():
+    print()
+    for i in range(len(data['.word'])):
+        print(hex((base_address + 4 * i)) + ": " + str(data['.word'][i]))
+    print()
 
 # main program
+
+text_address = 0x00000000
+text_memory = ['' for i in range(2048)]
+base_memory = 0 * [2048]
+
 fileopen = open("programs/example1.asm", "r+")
 instructionslist = all_instructions(fileopen)
 
 data_and_text = {'data': [], 'text': []}
-label ={'data':[]}          #list containing data section labels
+label = {'data': []}  # list containing data section labels
 # dictionary containing two lists data list and text list which contain respective instructions
 data_index = 0
 text_index = 0
@@ -292,11 +324,10 @@ for i in range(len(instructionslist)):
         text_index = i
 
 for i in range(data_index + 1, text_index):
-    if(len(instructionslist[i])>1):
-         data_and_text['data'].append(instructionslist[i])
-    elif(len(instructionslist[i])==1):
-         label['data'].append(instructionslist[i])
-
+    if (len(instructionslist[i]) > 1):
+        data_and_text['data'].append(instructionslist[i])
+    elif (len(instructionslist[i]) == 1):
+        label['data'].append(instructionslist[i])
 
 for i in range(text_index + 1, len(instructionslist)):
     if instructionslist[i][0] != 'main:':
@@ -304,13 +335,16 @@ for i in range(text_index + 1, len(instructionslist)):
 
 data = {'.word': []}
 # count = 0
+# print(data_and_text['data'][0])
 for ins in data_and_text['data']:
     if ins[0] == '.word':
         for i in range(1, len(ins)):
             data['.word'].append(int(ins[i]))
+            base_memory.append(int(ins[i]))
     elif ins[1] == '.word':
-       for i in range(2, len(ins)):
-              data['.word'].append(int(ins[i]))
+        for i in range(2, len(ins)):
+            data['.word'].append(int(ins[i]))
+            base_memory.append(int(ins[i]))   #b
         # count += 1
 
 main = {}  # dictionary which contains labels as keys and corresponding numbers as values
@@ -327,79 +361,77 @@ for ins in data_and_text['text']:
     if ins[0] in main.keys():
         data_and_text['text'].remove(ins)
 
+index = 0
 for ins in data_and_text['text']:
-    print(ins)  # prints list of all list instructions without labels
+    text_memory[index] = ins  # storing each instruction in text segment
+    print(str(index) + ":  " + str(text_memory[index]))
+    # prints list of all list instructions without labels
+    index += 1
 
 print(Reg)  # prints Reg list
 print(data['.word'])  # prints elements of array
 print(data_and_text['data'])
 print(main)  # prints labels inside text part after main
-print("Press 1 to RUN file")
+
+print("\nPress 1 to RUN file")
 print("Press 2 to RUN step by step")
+print("Press 3 to Exit")
 
-choice = int(input())
-count = 0
-# print(len(data_and_text['text']))
-
-if choice == 1:
-    while PC != len(data_and_text['text']):
-        print(PC)
-        count += 1
-        #   print(data_and_text['text'][PC])
-        PC = perform_instructions(data_and_text['text'][PC], PC)
-
-        if PC > len(data_and_text['text']):
-            print("Sorry terminating the program due to unexpected error :(")
-
-    for Registers in Reg.keys():
-        print(Registers + ": " + str(Reg[Registers]))
-        print()
-
-    for i in range(len(data['.word'])):
-        print(hex((base_address + 4 * i)) + ": " + str(data['.word'][i]))
-        print()
-
-    print("Total count: " + str(count))
-
-elif choice == 2:
+while True:
+    choice = int(input())
     count = 0
-    while True:  # keeps on running until we stop giving some number
-        print('1.Run Step by Step || 2.Show registers || 3.Show Memory || 4.Run file || 5.exit')
-        option = int(input())
-        if option == 1:
-            if PC != len(data_and_text['text']):
-                print(PC)
-                count += 1
-                print(data_and_text['text'][PC])
-                PC = perform_instructions(data_and_text['text'][PC], PC)
-                if PC > len(data_and_text['text']):
-                    exit("Sorry terminating the program due to unexpected error :(")
-                continue
-            print("Executed the file Successfully...")
-            print(count)
+    # print(len(data_and_text['text']))
+    if choice == 1:
+        while PC != len(data_and_text['text']):
+            #  print(PC)
+            count += 1
+            #   print(data_and_text['text'][PC])
+            PC = perform_instructions(data_and_text['text'][PC], PC)
 
-        elif option == 2:
-            for register in Reg.keys():
-                print(register + ": " + str(Reg[register]))
+            if PC > len(data_and_text['text']):
+                print("Sorry terminating the program due to unexpected error :(")
+        show_registers()
+        show_memory()
+        print("Executed the file Successfully...")
+        print("Total count: " + str(count))
+    elif choice == 2:
+        count = 0
+        while True:  # keeps on running until we stop giving some number
+            print('1.Run Step by Step || 2.Show registers || 3.Show Memory || 4.Run file || 5.exit')
+            option = int(input())
+            if option == 1:
+                if PC != len(data_and_text['text']):
+                    print(PC)
+                    count += 1
+                    print(data_and_text['text'][PC])
+                    PC = perform_instructions(data_and_text['text'][PC], PC)
+                    if PC > len(data_and_text['text']):
+                        exit("Sorry terminating the program due to unexpected error :(")
+                    continue
+                else:
+                    print("Executed the file Successfully...")
+                    print(count)  # total count
+            elif option == 2:
+                show_registers()
+            elif option == 3:
+                show_memory()
+            elif option == 4:
+                while PC != len(data_and_text['text']):
+                    print(PC)
+                    count += 1
+                    #   print(data_and_text['text'][PC])
+                    PC = perform_instructions(data_and_text['text'][PC], PC)
+                    if PC > len(data_and_text['text']):
+                        print("Sorry terminating the program due to unexpected error :(")
                 print()
-        elif option == 3:
-            for i in range(len(data['.word'])):
-                print(hex((base_address + 4 * i)) + ": " + str(data['.word'][i]))
+                print("Executed the file Successfully...")
+                print("Total count: " + str(count))
                 print()
-        elif option == 4:
-            while PC != len(data_and_text['text']):
-                print(PC)
-                count += 1
-                #   print(data_and_text['text'][PC])
-                PC = perform_instructions(data_and_text['text'][PC], PC)
-                if PC > len(data_and_text['text']):
-                    print("Sorry terminating the program due to unexpected error :(")
-
-            print("Executed the file Successfully...")
-            print("Total count: " + str(count))
-
-        elif option == 5:
-            sys.exit("Exiting from the Program...")
-
-        else:
-            print("Unexpected Error occurs, Enter again...")
+            elif option == 5:
+                sys.exit("Exiting from the Program...")
+            else:
+                print("Unexpected Error occurs, Enter again...")
+    elif choice == 3:
+        sys.exit("Exiting from the Program...")
+    else:
+        print("\nUnexpected Error occurs, Enter again...\n")
